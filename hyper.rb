4ce0aab59694dbe40ec8cc3b97e962bbe193c9c6
Henrik Mühe <muehe@in.tum.de>
2012-07-04 16:16:36 +0200

    fixed gcc dep in hyper

--- hyper.rb
+++ hyper.rb
@@ -6,7 +6,6 @@ class Hyper < Formula
   md5 "ab7cf30719b2a5b8950ad48e2d37caa0"
 
   depends_on "llvm"
-  depends_on "gcc"
   depends_on "ld64"
   depends_on "clang-as-wrapper"
 
@@ -16,5 +15,16 @@ class Hyper < Formula
 
   def test
     system "false"
+  end
+
+  def caveats; <<-EOS.undent
+      HyPer requires a recent gcc, for instance 4.7. To install this, you
+      should tap into Homebrew/dupes and install gcc there. Since the installer
+      seems to be broken right now, gcc is not installed automatically as
+      a dependency but instead needs to be build by you using for instance:
+
+      brew tap Homebrew/dupes
+      brew install gcc --with-llvm --enable-cxx
+    EOS
   end
 end
